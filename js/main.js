41fcb89d00f2f20fa36a4094c519c7fee13ec1c4
JYabes <jonathanyabes@gmail.com>
2023-10-14 16:12:19 -0400

    Updating the index, javascript, and added new files under abstracts
    Updating the index, javascript, and added new files under abstracts

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// BURGER MENU
+
+const menu_btn=document.querySelector('.hamburger');
+const mobile_menu=document.querySelector('.mobile-nav2');
+
+menu_btn.addEventListener('click', function() {
+    menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
+});

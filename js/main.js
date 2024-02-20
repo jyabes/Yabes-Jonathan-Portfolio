3220973d55f7cb6e532edfc05660a37df5df5202
JYabes <jonathanyabes@gmail.com>
2024-02-20 16:15:13 -0500

    Updates
    Updated javascript, index.html, all project html, added animation, css...

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-// BURGER MENU
 
 const menu_btn=document.querySelector('.hamburger');
-const mobile_menu=document.querySelector('.mobile-nav2');
+const mobile_menu=document.querySelector('.mobile-nav');
 
 menu_btn.addEventListener('click', function() {
+    console.log('Hamburger Clicked!');
     menu_btn.classList.toggle('is-active');
     mobile_menu.classList.toggle('is-active');
 });
 
 // PLAYER
 const player = new Plyr('video');
-
-
 
